c8fa20656298675d78c87d6cb0ccb3a703e84298
Faraz Aslam <farazmalik52@gmail.com>
2020-04-13 20:32:14 +0500

    crud-rest-api-tested

---
+++ app/Http/Controllers/itemsController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Item;
+use Validator;
+
+class itemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $items=Item::all();
+        return response()->json($items);
+
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validator=Validator::make($request->all(),['text'=>'required','body'=>'required']);
+        if($validator->fails())
+        {
+            $response=array('response'=>$validator->messages(),'success'=>false);
+            return $response;
+        }
+        else
+        {
+            $item=new Item();
+            $item->text=$request->text;
+            $item->body=$request->body;
+            $item->save();
+            return response()->json($item);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $items=Item::find($id);
+        return response()->json($items);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validator=Validator::make($request->all(),['text'=>'required','body'=>'required']);
+        if($validator->fails())
+        {
+            $response=array('response'=>$validator->messages(),'success'=>false);
+            return $response;
+        }
+        else
+        {
+            $item=Item::find($id);
+            $item->text=$request->text;
+            $item->body=$request->body;
+            $item->save();
+            return response()->json($item);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $item=Item::find($id);
+        $item->delete();
+        $response=array('response'=>'item deleted','success'=>true);
+        return $response;
+    }
+}
